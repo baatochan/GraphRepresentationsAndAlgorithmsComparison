c998566a76da0e6db43e0894b95c492ba3fcab57
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-05-19 16:19:21 +0200

    Add basic program class to run UI

---
+++ Program.h
@@ -0,0 +1,21 @@
+//
+// Created by barto on 19.05.18.
+//
+
+#ifndef GRAPHREPRESENTATIONSANDALGORITHMSCOMPARISON_PROGRAM_H
+#define GRAPHREPRESENTATIONSANDALGORITHMSCOMPARISON_PROGRAM_H
+
+
+#include "DirectedGraph.h"
+#include "UndirectedGraph.h"
+
+class Program {
+private:
+	Graph *graph;
+
+public:
+	void start();
+};
+
+
+#endif //GRAPHREPRESENTATIONSANDALGORITHMSCOMPARISON_PROGRAM_H
