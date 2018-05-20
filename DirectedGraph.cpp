e1b6d03de0c792b2a86b79da6707b5fe1d8b9ddf
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-05-21 00:33:51 +0200

    Add Dijkstra's algorithm on list

--- DirectedGraph.cpp
+++ DirectedGraph.cpp
@@ -4,6 +4,7 @@
 
 #include <cmath>
 #include <random>
+#include <climits>
 #include "DirectedGraph.h"
 
 using namespace std;
@@ -152,5 +153,73 @@ std::string DirectedGraph::dijkstrasAlgorithmOnMatrix(int beginVertex, int endVe
 }
 
 std::string DirectedGraph::dijkstrasAlgorithmOnList(int beginVertex, int endVertex) {
-	throw "Algorytm jeszcze nie zaimplementowany!";
+	if (adjacencyList.size() == 0)
+		throw "Graf pusty!";
+
+	int numberOfVertices = adjacencyList.size();
+
+
+	if (beginVertex >= numberOfVertices || endVertex >= numberOfVertices)
+		throw "Poczatkowy lub koncowy wierzcholek nie istnieje!";
+
+	// create needed arrays and assign default values
+	vector<bool> visitedVertices;
+	vector<unsigned long> pathLength;
+	vector<int> previousVertex;
+	visitedVertices.assign(numberOfVertices, false);
+	pathLength.assign(numberOfVertices, ULONG_MAX);
+	previousVertex.assign(numberOfVertices, -1);
+
+	// assaing starting value
+	pathLength[beginVertex] = 0;
+	int currentVertex = beginVertex;
+	unsigned long shortestPath;
+	int shortestPathVertex;
+
+	// run main loop for numberOfVertices times (every vertex will be visisted)
+	for (int i = 0; i < numberOfVertices; i++) {
+		unsigned long currentLength = pathLength[currentVertex];
+
+		for (auto& edge : adjacencyList[currentVertex]) {
+			if (pathLength[edge.edgeEnd] > currentLength + edge.value){
+				pathLength[edge.edgeEnd] = currentLength + edge.value;
+				previousVertex[edge.edgeEnd] = currentVertex;
+			}
+		}
+
+		visitedVertices[currentVertex] = true;
+
+		shortestPath = ULONG_MAX;
+		shortestPathVertex = -1;
+
+		for (int j = 0; j < numberOfVertices; j++) {
+			if (!visitedVertices[j]) {
+				if (pathLength[j] < shortestPath) {
+					shortestPath = pathLength[j];
+					shortestPathVertex = j;
+				}
+			}
+		}
+
+		if ((i != numberOfVertices - 1) && (shortestPathVertex == -1))
+			throw "Graf niespojny!";
+
+		currentVertex = shortestPathVertex;
+
+	}
+
+	string output;
+	output = "Najkrotsza droga z wierzch.: " + to_string(beginVertex) + " do wierzch.: " + to_string(endVertex) + " wynosi: " + to_string(pathLength[endVertex]) + ".\n";
+	output += "Prowadzi nastepujaca droga: ";
+
+	currentVertex = endVertex;
+
+	output += to_string(currentVertex);
+
+	while (currentVertex != beginVertex) {
+		currentVertex = previousVertex[currentVertex];
+		output +=  " <- " + to_string(currentVertex);
+	}
+
+	return output;
 }
